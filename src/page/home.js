fc6fade862d321d5f278fcc83601768776f8f726
智 <z1501380176@163.com>
2020-06-22 11:02:15 +0800

    添加 首页页面

---
+++ src/page/home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class Test extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+    componentWillMount(){
+
+    }
+    render() {
+        return (
+            <div>欢迎您登录</div> 
+        )
+    }
+}
