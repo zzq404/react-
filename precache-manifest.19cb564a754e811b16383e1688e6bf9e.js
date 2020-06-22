11e29e3ab1861f9d4221adba14a132846b3c8e4f
智 <z1501380176@163.com>
2020-06-22 17:41:33 +0800

    Updates

---
+++ precache-manifest.19cb564a754e811b16383e1688e6bf9e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa64d2aafad1be248629b7e727a327a",
+    "url": "/react-admin/index.html"
+  },
+  {
+    "revision": "ba5b6905345600477b72",
+    "url": "/react-admin/static/css/13.a2026422.chunk.css"
+  },
+  {
+    "revision": "f56840ef489f061e04a4",
+    "url": "/react-admin/static/css/main.932e4bf0.chunk.css"
+  },
+  {
+    "revision": "226e9521b55937bf3e53",
+    "url": "/react-admin/static/js/0.aa52baf9.chunk.js"
+  },
+  {
+    "revision": "22550922cf0dd9d90e96",
+    "url": "/react-admin/static/js/1.b4dfcb24.chunk.js"
+  },
+  {
+    "revision": "cec4dc19707e3647ff0b46bcc9936b37",
+    "url": "/react-admin/static/js/1.b4dfcb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d738e0d0e2323d5cd0f",
+    "url": "/react-admin/static/js/10.1211547e.chunk.js"
+  },
+  {
+    "revision": "ba5b6905345600477b72",
+    "url": "/react-admin/static/js/13.4fc4cd81.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-admin/static/js/13.4fc4cd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f95b434d3f9a533aa198",
+    "url": "/react-admin/static/js/14.2b842b65.chunk.js"
+  },
+  {
+    "revision": "7a2b3e281bdd9b71aa1d",
+    "url": "/react-admin/static/js/15.5c799046.chunk.js"
+  },
+  {
+    "revision": "19d3fe42c6c8df7acec0",
+    "url": "/react-admin/static/js/2.f39d6450.chunk.js"
+  },
+  {
+    "revision": "ebb8482b22703fea4ea45ed01fc49ea0",
+    "url": "/react-admin/static/js/2.f39d6450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869beb54158783761b78",
+    "url": "/react-admin/static/js/3.b8193628.chunk.js"
+  },
+  {
+    "revision": "e1ea14054c833b0e566d",
+    "url": "/react-admin/static/js/4.c3a1a467.chunk.js"
+  },
+  {
+    "revision": "6d19ac05a6e6b6db178c",
+    "url": "/react-admin/static/js/5.4c956273.chunk.js"
+  },
+  {
+    "revision": "4cd517e7e9e60f3778c4",
+    "url": "/react-admin/static/js/6.d0cb6a43.chunk.js"
+  },
+  {
+    "revision": "427a5e277b12bbb4db49",
+    "url": "/react-admin/static/js/7.994d125d.chunk.js"
+  },
+  {
+    "revision": "f0725e05b0862320e9ac",
+    "url": "/react-admin/static/js/8.6bc27e29.chunk.js"
+  },
+  {
+    "revision": "b7c427989d9e2b327eac",
+    "url": "/react-admin/static/js/9.40d77ad8.chunk.js"
+  },
+  {
+    "revision": "f56840ef489f061e04a4",
+    "url": "/react-admin/static/js/main.d6762f11.chunk.js"
+  },
+  {
+    "revision": "62cbd2d383ac1855f42c",
+    "url": "/react-admin/static/js/runtime-main.e1170b4e.js"
+  },
+  {
+    "revision": "0899ffa6d98e086a98b5c9282d123434",
+    "url": "/react-admin/static/media/login-bg.0899ffa6.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-admin/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4c5da7c5297dc474883c67c290b57a54.js"
+  "/react-admin/precache-manifest.19cb564a754e811b16383e1688e6bf9e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-admin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
